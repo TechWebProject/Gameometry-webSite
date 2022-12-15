0ed1726007715797aa555e4d3c8526714e29d73a
MikeIsBack <michael.amista@studenti.unipd.it>
2022-12-15 16:28:47 +0100

    aggiunti nuovi file su cui lavorare

---
+++ DBProva/Codice/videogiochi.php
@@ -0,0 +1,54 @@
+<?php
+include "Componenti/connect.php";
+
+$doc = new DOMDocument;
+$html = $doc->appendChild($doc->createElement('html'));
+$html->setAttribute('lang','it');
+$body = $doc->appendChild($doc->createElement('body'));
+
+//head
+$head=file_get_contents('sezioniComuni/head.html');
+$head = str_replace("<title>Gameometry</title>","<title>Gameometry - Videogiochi</title>", $head);
+$html->appendChild($doc->createTextNode($head));
+
+//header
+$header=file_get_contents('sezioniComuni/header.html');
+$header = str_replace("Notizie","Videogiochi",$header);
+$body->appendChild($doc->createTextNode($header));
+
+//Codice php generazione giochi
+$main = $body->appendChild($doc->createElement('main'));
+
+$h1_Video = $main->appendChild($doc->createElement('h1'));
+$h1_Video->setAttribute('id','newsTitle');
+$h1_Video = $h1_Video->appendChild($doc->createTextNode('VIDEOGIOCHI'));
+
+$div_sezioneVideogiochi = $main->appendChild($doc->createElement('div'));
+$div_sezioneVideogiochi->setAttribute('id','sezioneVideogiochi');
+
+$dir = scandir("imgs");
+$str = 'imgs/';
+
+$imgz = array(count($dir)-2);
+
+for($i = 2; $i < count($dir); $i++){
+    $res = $str.$dir[$i];
+    $trimmedword = RemoveSpecialChar($dir,$i);
+    $imgz[$i-2]=array('src' => $res , 'alt' => $trimmedword);
+}
+
+foreach ($imgz as $attributes) {
+    $imgtag = $div_sezioneVideogiochi->appendChild($doc->createElement('img'));
+    foreach ($attributes as $key => $value) {
+        $imgtag->setAttribute($key, $value);
+    }
+    $imgtag->setAttribute('class','imgs');
+}
+
+//footer
+$footer=file_get_contents("sezioniComuni/footer.html");
+$body->appendChild($doc->createTextNode($footer));
+
+$doc->formatOutput=true;
+echo html_entity_decode($doc->saveHTML(),ENT_QUOTES,"UTF-8");
+?>
---
+++ DBProva/ReadME.txt
@@ -0,0 +1,31 @@
+==================
+UTILIZZO XAMPP
+==================
+
+1- Scaricare Xampp
+2- Andare sulla directory di Xampp (solitamente su C: ) , in htdocs spostare la cartella col progetto (DBprova)
+3- Avviare Xampp e cliccare su Start su Apache e su MySQL
+4- Aprire il browser e scrivere sulla barra di ricerca localhost/DBProva/index.html
+
+
+======================
+LETTURA DIRECTORY
+======================
+
+-Immagini: cartella che corrisponde alla cartella attuale di GitHub per le immagini utilizzate nel progetto
+
+-imgs: cartella utilizzata dal codice per reperire le img attualmente presenti in locale ma anche sul DB
+
+-connect.php: file importato dove serve per connettersi al DB + una funzione per rimuovere dai nome le estensioni dei file
+
+-index.html: pagina fittizia in quanto mi serviva solo l'utilizzo del  bottone che rievoca l'utilizzo dell'li "videogiochi" nell'header
+
+-*autoinsert: codice da runnare da vs non appena si aggiunge una nuova immagine sulla cartella imgs. Invia una query che inserisce sul db le sue info (fin ora utili)
+
+-*deletephotosDBandDIR: codice da runnare da vs non appena si decide di rimuovere un'immagine, lo si avvia e si specifica su console l'esatto nome del file senza estensioni (corrisponde all'attributo "titolo" sul DB)
+
+-css vari soliti non aggiornati all'ultima modifica
+
+
+
+*per utilizzare questi file è necessario avere il DB "gameometry" creato con la tabella "videogioco" e due attributi di testo "titolo" e "imgLocandina"
