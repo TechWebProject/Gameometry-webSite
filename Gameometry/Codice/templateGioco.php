d0480fc51e0a1f2d85bc2273ecdb8a8be8f4093d
MikeIsBack <michael.amista@studenti.unipd.it>
2022-12-20 23:33:24 +0100

    fix videogiochi e aggiunta banner

--- Gameometry/Codice/templateGioco.php
+++ Gameometry/Codice/templateGioco.php
@@ -9,7 +9,6 @@ $body = $doc->appendChild($doc->createElement('body'));
 //head
 $head = file_get_contents('sezioniComuni/head.html');
 $head = str_replace("<title>Gameometry</title>", "<title>Gameometry | Videogioco</title>", $head);
-//Cambiare con il nome del gioco (riga sopra e sotto)
 $head = str_replace("Pagina dedicata alle informazioni di un singolo utente", "Pagina dedicata a uno specifico videogioco", $head);
 $head = str_replace("videogioco, videogiochi, utente, recensioni", "gameometry, videogioco, videogiochi, utente, recensioni", $head);
 $html->appendChild($doc->createTextNode($head));
@@ -27,7 +26,7 @@ $html->appendChild($doc->createTextNode($header));
 //QUERY
 $db1=OpenCon();
 $title=mysqli_real_escape_string($db1,$title);
-$tmpquery= "SELECT trama,rilascio,casaProduttrice,imgLocandina,piattaformaV,genereV FROM videogioco WHERE titolo='$title'";
+$tmpquery= "SELECT trama,rilascio,casaProduttrice,imgBanner,imgLocandina,piattaformaV,genereV FROM videogioco WHERE titolo='$title'";
 
 $result = mysqli_query($db1,$tmpquery);
 
@@ -36,6 +35,7 @@ $r = mysqli_fetch_all($result,MYSQLI_ASSOC);
 $trama = $r[0]['trama'];
 $data_uscita=$r[0]['rilascio'];
 $publisher=$r[0]['casaProduttrice'];
+$banner=$r[0]['imgBanner'];
 $imgLocandina=$r[0]['imgLocandina'];
 $piattaforma=$r[0]['piattaformaV'];
 $genere=$r[0]['genereV'];
@@ -46,14 +46,15 @@ CloseCon($db1);
 $main = $html->appendChild($doc->createElement('main'));
 $imgbanner = $html->appendChild($doc->createElement('img'));
 $imgbanner->setAttribute('id', 'bannergioco');
-$imgbanner->setAttribute('alt', 'Immagine di copertina');
-$imgbanner->setAttribute('src', "Banner/banner_gtav.jpeg"); //(da cambiare con il database)
+$tmpBannerName = str_replace("Banner/","",$banner);
+$imgbanner->setAttribute('alt', $tmpBannerName); /* togliere estensione img */
+$imgbanner->setAttribute('src', $banner);
 $main->appendChild($imgbanner);
 
 $h1 = $html->appendChild($doc->createElement('h1'));
 $h1->setAttribute('id', 'titologioco');
 $title=str_replace("\'","'",$title);
-$h1->appendChild($doc->createTextNode($title)); //DATABASE
+$h1->appendChild($doc->createTextNode($title)); 
 $main->appendChild($h1);
 
 $lista = $html->appendChild($doc->createElement('dl'));
@@ -62,8 +63,9 @@ $main->appendChild($lista);
 
 $imglocandina = $html->appendChild($doc->createElement('img'));
 $imglocandina->setAttribute('id', 'locandinagioco');
-$imglocandina->setAttribute('alt', 'locandina del gioco');
-$imglocandina->setAttribute('src', $imgLocandina); //(da cambiare con il database)
+$tmpImgName = str_replace("Locandina/","",$imgLocandina);
+$imglocandina->setAttribute('alt', $tmpImgName); /* togliere estensione img */
+$imglocandina->setAttribute('src', $imgLocandina);
 $lista->appendChild($imglocandina);
 
 $listaattributi = $html->appendChild($doc->createElement('dl'));
@@ -75,7 +77,7 @@ $attributo1->appendChild($doc->createTextNode("Publisher"));
 $listaattributi->appendChild($attributo1);
 //
 $attributo1db = $html->appendChild($doc->createElement('dd'));
-$attributo1db->appendChild($doc->createTextNode($publisher)); //DATABASE
+$attributo1db->appendChild($doc->createTextNode($publisher)); 
 $listaattributi->appendChild($attributo1db);
 //
 $attributo2 = $html->appendChild($doc->createElement('dt'));
@@ -83,23 +85,23 @@ $attributo2->appendChild($doc->createTextNode("Data di uscita"));
 $listaattributi->appendChild($attributo2);
 //
 $attributo2db = $html->appendChild($doc->createElement('dd'));
-$attributo2db->appendChild($doc->createTextNode($data_uscita)); //DATABASE
+$attributo2db->appendChild($doc->createTextNode($data_uscita)); 
 $listaattributi->appendChild($attributo2db);
 //
 $attributo3 = $html->appendChild($doc->createElement('dt'));
-$attributo3->appendChild($doc->createTextNode("Piattaforma")); //DATABASE
+$attributo3->appendChild($doc->createTextNode("Piattaforma")); 
 $listaattributi->appendChild($attributo3);
 //
 $attributo3db = $html->appendChild($doc->createElement('dd'));
-$attributo3db->appendChild($doc->createTextNode($piattaforma)); //DATABASE
+$attributo3db->appendChild($doc->createTextNode($piattaforma)); 
 $listaattributi->appendChild($attributo3db);
 //
 $attributo4 = $html->appendChild($doc->createElement('dt'));
-$attributo4->appendChild($doc->createTextNode("Genere")); //DATABASE
+$attributo4->appendChild($doc->createTextNode("Genere")); 
 $listaattributi->appendChild($attributo4);
 //
 $attributo4db = $html->appendChild($doc->createElement('dd'));
-$attributo4db->appendChild($doc->createTextNode($genere)); //DATABASE
+$attributo4db->appendChild($doc->createTextNode($genere)); 
 $listaattributi->appendChild($attributo4db);
 //
 
@@ -123,11 +125,11 @@ $scores->appendChild($rec);
 
 $puntegiocritica = $html->appendChild($doc->createElement('span'));
 $puntegiocritica->setAttribute('id', 'punteggioCritica');
-$puntegiocritica->appendChild($doc->createTextNode('null')); //DATABASE
+$puntegiocritica->appendChild($doc->createTextNode('null')); 
 $rec->appendChild($puntegiocritica);
 
 $nostropunteggio = $html->appendChild($doc->createElement('span'));
-$nostropunteggio->appendChild($doc->createTextNode('Il nostro punteggio')); //DATABASE
+$nostropunteggio->appendChild($doc->createTextNode('Il nostro punteggio')); 
 $rec->appendChild($nostropunteggio);
 
 /*
@@ -147,7 +149,7 @@ $scoresutenti->appendChild($recutenti);
 
 $punteggioutenti = $html->appendChild($doc->createElement('span'));
 $punteggioutenti->setAttribute('id', 'punteggioUtenti');
-$punteggioutenti->appendChild($doc->createTextNode('null')); //DATABASE
+$punteggioutenti->appendChild($doc->createTextNode('null')); 
 $recutenti->appendChild($punteggioutenti);
 
 $punteggioutentispan = $html->appendChild($doc->createElement('span'));
