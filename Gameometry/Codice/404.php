973ae62db1a987c4f515a920df495eb7b0854e7e
fliposab <filipposabbadin.2001@gmail.com>
2022-12-21 11:52:37 +0100

    Aggiunta pagina 404

--- Gameometry/Codice/404.php
+++ Gameometry/Codice/404.php
@@ -1,15 +1,17 @@
 <?php
 
+http_response_code(404);
+
 $doc = new DOMDocument;
 $html = $doc->appendChild($doc->createElement('html'));
 $html->setAttribute('lang', 'it');
 $body = $doc->appendChild($doc->createElement('body'));
 
 //head
-$head=file_get_contents('sezioniComuni/head.html');
-$head = str_replace("<title>Gameometry</title>","Error 404", $head);
+$head = file_get_contents('sezioniComuni/head.html');
+$head = str_replace("<title>Gameometry</title>", "Error 404", $head);
 $head = str_replace("Pagina dedicata alle informazioni di un singolo utente", "Pagina di errore", $head);
-$head = str_replace("Stili/style.css", "Stili/style404.php", $head);
+$head = str_replace("Stili/style.css", "./Stili/style404.css", $head);
 $head = str_replace("<meta name=&quot;keywords&quot; content=&quot;videogioco, videogiochi, utente, recensioni&quot; />", "", $head);
 $html->appendChild($doc->createTextNode($head));
 
@@ -18,5 +20,3 @@ $html->appendChild($doc->createTextNode($file));
 
 $doc->formatOutput = true;
 echo html_entity_decode($doc->saveHTML(), ENT_QUOTES, "UTF-8");
-
-?>
