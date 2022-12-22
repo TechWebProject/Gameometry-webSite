4a76192ab7ce6b0e4f12a94e0562d06bd3203a6b
fliposab <filipposabbadin.2001@gmail.com>
2022-12-22 14:35:32 +0100

    Aggiunto Errore 500-507

---
+++ Gameometry/Codice/500.php
@@ -0,0 +1,22 @@
+<?php
+
+http_response_code(500);
+
+$doc = new DOMDocument;
+$html = $doc->appendChild($doc->createElement('html'));
+$html->setAttribute('lang', 'it');
+$body = $doc->appendChild($doc->createElement('body'));
+
+//head
+$head = file_get_contents('sezioniComuni/head.html');
+$head = str_replace("<title>Gameometry</title>", "Error 500", $head);
+$head = str_replace("Pagina dedicata alle informazioni di un singolo utente", "Pagina di errore", $head);
+$head = str_replace("Stili/style.css", "./Stili/style500.css", $head);
+$head = str_replace("<meta name=&quot;keywords&quot; content=&quot;videogioco, videogiochi, utente, recensioni&quot; />", "", $head);
+$html->appendChild($doc->createTextNode($head));
+
+$file = file_get_contents("Componenti/template500.html");
+$html->appendChild($doc->createTextNode($file));
+
+$doc->formatOutput = true;
+echo html_entity_decode($doc->saveHTML(), ENT_QUOTES, "UTF-8");
