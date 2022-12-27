5c198662abf113afc4fdb55acec2a705836ef9f6
Fabio Pantaleo <pantaleo.fabio01@gmail.com>
2022-12-27 21:14:35 +0100

    Fase 1 Ricerca

--- Gameometry/Codice/Componenti/scriptSearch.js
+++ Gameometry/Codice/Componenti/scriptSearch.js
@@ -16,27 +16,41 @@ inputBox.addEventListener("input",function(e){
   }
   if(userData){
     emptyArray = emptyArray.filter((data) =>{
-      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+      return data.toLocaleLowerCase().search(userData.toLocaleLowerCase());
     });
 
     emptyArray = emptyArray.map((data)=>{
         return data= '<li>' + data + '</li>';
     });
 
+    searchWrapper.classList.add("active");
+    suggBox.classList.add("active");
     
-  }else{
+    showSuggestions(emptyArray);
 
+    let allList = suggBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++) {
+      allList[i].setAttribute("onclick", "select(this)");
   }
-
-  showSuggestions(emptyArray);
-
+  }else{
+    searchWrapper.classList.remove("active");
+    suggBox.classList.remove("active");
+    suggBox.innerHTML = [];
+  }
+  
 });
 
+function select(element){
+  let selectData = element.textContent;
+  inputBox.value = selectData;
+  searchWrapper.classList.remove("active");
+}
 
 function showSuggestions(list){
   let listData;
   if(!list.length){
-
+    userValue = inputBox.value;
+    listData = '<li>' + userValue + '</li>';
   }else{
     listData = list.join('');
     suggBox.innerHTML = listData;
