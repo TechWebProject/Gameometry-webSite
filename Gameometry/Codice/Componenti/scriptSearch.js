445090f673ba7d3ce6b5b485d5a8d88ed59bfc2d
Fabio Pantaleo <pantaleo.fabio01@gmail.com>
2022-12-27 20:30:38 +0100

    bozza 2.0 ricerca

--- Gameometry/Codice/Componenti/scriptSearch.js
+++ Gameometry/Codice/Componenti/scriptSearch.js
@@ -3,16 +3,42 @@
 const searchWrapper = document.querySelector("#ricerca");
 
 const inputBox = document.getElementById("searchBar");
+const suggBox = document.querySelector(".autocom-box");
 
 var arrTitoli = document.getElementById("arrTitoli").textContent;
 var titoli = arrTitoli.split(",");
 
 inputBox.addEventListener("input",function(e){
-  
+  let userData = e.target.value;
   let emptyArray= [];
   for(var i=0; i<titoli.length; i++){
     emptyArray.push(titoli[i]);
+  }
+  if(userData){
+    emptyArray = emptyArray.filter((data) =>{
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+
+    emptyArray = emptyArray.map((data)=>{
+        return data= '<li>' + data + '</li>';
+    });
+
+    
+  }else{
 
   }
 
+  showSuggestions(emptyArray);
+
 });
+
+
+function showSuggestions(list){
+  let listData;
+  if(!list.length){
+
+  }else{
+    listData = list.join('');
+    suggBox.innerHTML = listData;
+  }
+}
