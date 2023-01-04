b3aad56c9c14e49b647953120bda42708e5cd4d1
fliposab <filipposabbadin.2001@gmail.com>
2023-01-04 16:27:35 +0100

    Migliorato lo stile delle pagine di errore

--- Gameometry/Codice/404.php
+++ Gameometry/Codice/404.php
@@ -3,6 +3,7 @@
 http_response_code(404);
 
 $doc = new DOMDocument;
+$doc->appendChild($doc->createTextNode('<!DOCTYPE html>'));
 $html = $doc->appendChild($doc->createElement('html'));
 $html->setAttribute('lang', 'it');
 $body = $doc->appendChild($doc->createElement('body'));
@@ -12,11 +13,13 @@ $head = file_get_contents('sezioniComuni/head.html');
 $head = str_replace("<title>Gameometry</title>", "Error 404", $head);
 $head = str_replace("Pagina dedicata alle informazioni di un singolo utente", "Pagina di errore", $head);
 $head = str_replace("Stili/style.css", "./Stili/style404.css", $head);
+$head = str_replace('<link rel="stylesheet" href="Stili/mini.css" media="handheld, screen and (max-width:820px), only screen and (max-device-width: 800px)" /> ', "", $head);
 $head = str_replace("<meta name=&quot;keywords&quot; content=&quot;videogioco, videogiochi, utente, recensioni&quot; />", "", $head);
 $html->appendChild($doc->createTextNode($head));
 
-$file = file_get_contents("Componenti/404.html");
+$file = file_get_contents("Componenti/template404.html");
 $html->appendChild($doc->createTextNode($file));
 
 $doc->formatOutput = true;
 echo html_entity_decode($doc->saveHTML(), ENT_QUOTES, "UTF-8");
+$doc->appendChild($doc->createTextNode('<!DOCTYPE html>'));
--- Gameometry/Codice/500.php
+++ Gameometry/Codice/500.php
@@ -4,6 +4,7 @@ http_response_code(500);
 
 $doc = new DOMDocument;
 $html = $doc->appendChild($doc->createElement('html'));
+$doc->appendChild($doc->createTextNode('<!DOCTYPE html>'));
 $html->setAttribute('lang', 'it');
 $body = $doc->appendChild($doc->createElement('body'));
 
@@ -12,6 +13,7 @@ $head = file_get_contents('sezioniComuni/head.html');
 $head = str_replace("<title>Gameometry</title>", "Error 500", $head);
 $head = str_replace("Pagina dedicata alle informazioni di un singolo utente", "Pagina di errore", $head);
 $head = str_replace("Stili/style.css", "./Stili/style404.css", $head);
+$head = str_replace('<link rel="stylesheet" href="Stili/mini.css" media="handheld, screen and (max-width:820px), only screen and (max-device-width: 800px)" /> ', "", $head);
 $head = str_replace("<meta name=&quot;keywords&quot; content=&quot;videogioco, videogiochi, utente, recensioni&quot; />", "", $head);
 $html->appendChild($doc->createTextNode($head));
 
@@ -20,3 +22,4 @@ $html->appendChild($doc->createTextNode($file));
 
 $doc->formatOutput = true;
 echo html_entity_decode($doc->saveHTML(), ENT_QUOTES, "UTF-8");
+$doc->appendChild($doc->createTextNode('</html>'));
