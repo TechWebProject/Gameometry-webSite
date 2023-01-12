df9fb5184500f26c05efe5985d01a70527603da6
MikeIsBack <michael.amista@studenti.unipd.it>
2023-01-12 11:37:15 +0100

    fix generale dello style

--- Gameometry/Codice/areaUtente.php
+++ Gameometry/Codice/areaUtente.php
@@ -118,7 +118,7 @@ function generateRandomString($length = 30) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
+    for($i = 0; $i < $length; $i++){
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
