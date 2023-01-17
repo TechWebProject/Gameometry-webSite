2b4d441856aea4ca4bb8d35e0ef5ebc95fc7c7de
MarkBridge11 <marco.brigo@studenti.unipd.it>
2023-01-17 17:24:33 +0100

    corrette pagine 404 e 500

--- Gameometry/Codice/404.php
+++ Gameometry/Codice/404.php
@@ -5,5 +5,12 @@ $template = new template();
 $template->setPage("template404.html");
 $errorpage = $template->initializePage();
 
+http_response_code(404);
+
+$errorpage = str_replace("Titolo_pagina","Error 404",$errorpage);
+$errorpage = str_replace("<link rel=\"stylesheet\" href=\"Stili/style.css\" media=\"handheld, screen\" />","<link rel=\"stylesheet\" href=\"Stili/style404.css\" media=\"handheld, screen\"/>",$errorpage);
+$errorpage = str_replace("<link rel=\"stylesheet\" href=\"Stili/print.css\" media=\"print\" />"," ",$errorpage);
+$errorpage = str_replace("<link rel=\"stylesheet\" href=\"Stili/mini.css\" media=\"handheld, screen and (max-width:820px), only screen and (max-device-width: 800px)\" /> "," ",$errorpage);
+
 echo $errorpage;
 ?>
--- Gameometry/Codice/500.php
+++ Gameometry/Codice/500.php
@@ -5,5 +5,12 @@ $template = new template();
 $template->setPage("template500.html");
 $errorpage = $template->initializePage();
 
+http_response_code(500);
+
+$errorpage = str_replace("Titolo_pagina","Error 500",$errorpage);
+$errorpage = str_replace("<link rel=\"stylesheet\" href=\"Stili/style.css\" media=\"handheld, screen\" />","<link rel=\"stylesheet\" href=\"Stili/style404.css\" media=\"handheld, screen\"/>",$errorpage);
+$errorpage = str_replace("<link rel=\"stylesheet\" href=\"Stili/print.css\" media=\"print\" />"," ",$errorpage);
+$errorpage = str_replace("<link rel=\"stylesheet\" href=\"Stili/mini.css\" media=\"handheld, screen and (max-width:820px), only screen and (max-device-width: 800px)\" /> "," ",$errorpage);
+
 echo $errorpage;
 ?>
