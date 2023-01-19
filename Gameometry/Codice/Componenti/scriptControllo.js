77c665a33cecddc05b0a01ff5a982d666179fc3c
MarkBridge11 <marco.brigo@studenti.unipd.it>
2023-01-19 11:55:00 +0100

    Inserito nell'head scriptcontrollo e slider

--- Gameometry/Codice/Componenti/scriptControllo.js
+++ Gameometry/Codice/Componenti/scriptControllo.js
@@ -1,54 +1,56 @@
 
-const checkEmail = document.getElementById("email");
-const errorEmail = document.getElementById("errorEmailRegister");
+function checkRegister(){
+    const checkEmail = document.getElementById("email");
+    const errorEmail = document.getElementById("errorEmailRegister");
 
-checkEmail.addEventListener("blur",function(e){
-    errorEmail.innerText="";
-    // \w parola a caso "marco"
-    // ([\.-]) marco. oppure marco-
-    // ?\w controlla se c'è una parola marco.brigo
-    // @ forzata
-    // \w parola => gmail
-    // ([\.-]) punto o trattino
-    // parola opzionale che c'è solo se c'è [. o -]
-    // poi il punto per forza 
-    // l'it o com
-    // $ => fine stringa (forse) o che deve finire
-    if(checkEmail.value.length>0&&checkEmail.value.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)!=0){
-        errorEmail.innerText="Sintassi della mail errata";
-    }
-});
+    checkEmail.addEventListener("blur",function(e){
+        errorEmail.innerText="";
+        // \w parola a caso "marco"
+        // ([\.-]) marco. oppure marco-
+        // ?\w controlla se c'è una parola marco.brigo
+        // @ forzata
+        // \w parola => gmail
+        // ([\.-]) punto o trattino
+        // parola opzionale che c'è solo se c'è [. o -]
+        // poi il punto per forza 
+        // l'it o com
+        // $ => fine stringa (forse) o che deve finire
+        if(checkEmail.value.length>0&&checkEmail.value.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)!=0){
+            errorEmail.innerText="Sintassi della mail errata";
+        }
+    });
 
-checkEmail.addEventListener("input",()=>{
-    if(checkEmail.value.length<1)  errorEmail.innerText="";
-});
+    checkEmail.addEventListener("input",()=>{
+        if(checkEmail.value.length<1)  errorEmail.innerText="";
+    });
 
-const checkUser = document.getElementById("username");
-const errorUser = document.getElementById("errorUsernameRegister");
+    const checkUser = document.getElementById("username");
+    const errorUser = document.getElementById("errorUsernameRegister");
 
-checkUser.addEventListener("blur",function(e){
-    errorUser.innerText="";
-    const specialChars = /[`@#$%^&*()+=\[\]{};':"\\|,<>\/?~]/;
-    if(checkUser.value.length>0&&checkUser.value.search(/^[a-zA-Z0-9!\-_.]{4,30}$/)!=0||specialChars.test(checkUser.value)){
-        errorUser.innerText="Sintassi del nickname errata";
-    }
-});
+    checkUser.addEventListener("blur",function(e){
+        errorUser.innerText="";
+        const specialChars = /[`@#$%^&*()+=\[\]{};':"\\|,<>\/?~]/;
+        if(checkUser.value.length>0&&checkUser.value.search(/^[a-zA-Z0-9!\-_.]{4,30}$/)!=0||specialChars.test(checkUser.value)){
+            errorUser.innerText="Sintassi del nickname errata";
+        }
+    });
 
-checkUser.addEventListener("input",()=>{
-    if(checkUser.value.length<1)  errorUser.innerText="";
-});
+    checkUser.addEventListener("input",()=>{
+        if(checkUser.value.length<1)  errorUser.innerText="";
+    });
 
-const checkPsw = document.getElementById("password");
-const errorPsw = document.getElementById("errorPasswordRegister");
+    const checkPsw = document.getElementById("password");
+    const errorPsw = document.getElementById("errorPasswordRegister");
 
-checkPsw.addEventListener("blur",function(e){
-    errorPsw.innerText="";
-    const specialChars = /[`@#%^&*()_+\-=\[\]{};':"\\|,.<>\/~]/;
-    if((checkPsw.value.length>0)&&(checkPsw.value.search(/^[a-zA-Z0-9]*\w{1,12}$/)!=0||specialChars.test(checkPsw.value))){
-        errorPsw.innerText="Non è possibile utilizzare spazi o singoli/doppi apici";
-    }
-});
+    checkPsw.addEventListener("blur",function(e){
+        errorPsw.innerText="";
+        const specialChars = /[`@#%^&*()_+\-=\[\]{};':"\\|,.<>\/~]/;
+        if((checkPsw.value.length>0)&&(checkPsw.value.search(/^[a-zA-Z0-9]*\w{1,12}$/)!=0||specialChars.test(checkPsw.value))){
+            errorPsw.innerText="Non è possibile utilizzare spazi o singoli/doppi apici";
+        }
+    });
 
-checkPsw.addEventListener("input",()=>{
-    if(checkPsw.value.length<1)  errorPsw.innerText="";
-});
+    checkPsw.addEventListener("input",()=>{
+        if(checkPsw.value.length<1)  errorPsw.innerText="";
+    });
+}
